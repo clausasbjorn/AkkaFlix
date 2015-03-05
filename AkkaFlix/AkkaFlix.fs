c18d54d243c74f165dec1140125bc12820e0e039
clausasbjorn <claus@braincell.no>
2015-03-05 16:26:26 +0800

    Switched to functional approach

--- AkkaFlix/AkkaFlix.fs
+++ AkkaFlix/AkkaFlix.fs
@@ -40,11 +40,17 @@
 namespace AkkaFlix
 
     open System
+    open System.Collections.Generic
     open Akka.Actor
+
     open Akka.FSharp
 
-    module AkkaFlix =
+    type PlayEvent = 
+        { User: string
+          Asset: string }
 
+    module AkkaFlix =
+    
         // Prepare some test data
         let users = [| "Jack"; "Jill"; "Tom"; "Jane"; "Steven"; "Jackie" |]
         let assets = [| "The Sting"; "The Italian Job"; "Lock, Stock and Two Smoking Barrels"; "Inside Man"; "Ronin" |]
@@ -53,22 +59,95 @@ namespace AkkaFlix
 
         // Send a Play-event with a randomly selected user and asset to the Player-actor.
         // Continue sending every time a key other than [Esc] is pressed.
-        let rec loop player =
-            
+        let rec feedPlayerWithEvents player =
             player <! { User = users.[rnd.Next(users.Length)] ; Asset = assets.[rnd.Next(assets.Length)] }    
-
             match Console.ReadKey().Key with
             | ConsoleKey.Escape -> ()
-            | _ -> loop player
+            | _ -> feedPlayerWithEvents player
 
         // Create an actor system and the root Player-actor and pass it to the
         // input loop to start sending Play-events to it.
         [<EntryPoint>]
         let main argv =    
+
+            // Create a user actor for a single user identified by username
+            let createUser parent username = 
+                spawn parent username
+                    (fun mailbox -> 
+                        let rec loop(user) = actor {
+                            let! message = mailbox.Receive()
+                            printfn "%s is watching %s" user message
+                            return! loop(user)
+                        }                        
+                        loop(username))
+
+            // Create users actor (keeps track of all users)
+            let createUsers parent = 
+                spawn parent "users" 
+                    (fun mailbox -> 
+                        // Find or add an actor for a user identified by username
+                        let rec findOrSpawn (users : Dictionary<string, ActorRef>) username =
+                            match users.ContainsKey(username) with
+                            | true -> users.[username]
+                            | false ->
+                                users.Add(username, createUser mailbox username)
+                                findOrSpawn users username
+
+                        // Send the asset to the user actor to update it
+                        let updateUser users user asset =
+                            (findOrSpawn users user) <! asset
+
+                        // Handle incoming messages
+                        let rec loop(users) = actor {
+                            let! message = mailbox.Receive()
+                            updateUser users message.User message.Asset
+                            return! loop(users)
+                        }
+                        loop(new Dictionary<string, ActorRef>()))
+
+            // Create reporting actor
+            let createReporting parent =
+                spawn parent "reporting" 
+                    (fun mailbox -> 
+                        // Update the statistics for the asset
+                        let registerView (counters : Dictionary<string, int>) asset =
+                            match counters.ContainsKey(asset) with
+                            | true -> counters.[asset] <- counters.[asset] + 1
+                            | false -> counters.Add(asset, 1)
+
+                        // Print the hi score
+                        let printReport counters =
+                            counters
+                            |> Seq.sortBy (fun (KeyValue(k, v)) -> -v) 
+                            |> Seq.iter (fun (KeyValue(k, v)) -> printfn "%d\t%s" v k)
+
+                        // Handle incoming messages
+                        let rec loop(counters) = actor {
+                            let! message = mailbox.Receive();
+                            registerView counters message.Asset
+                            printReport counters
+                            return! loop(counters)
+                        }
+                        loop(new Dictionary<string, int>()))
+
+            // Create our Akka.NET actor system
             let system = System.create "akkaflix" (Configuration.load())
-            let player = system.ActorOf(Props(typedefof<Player>, Array.empty))
-    
-            loop player
+
+            // Our "root" actor that receives play events and pass them
+            // to "reporting" and "users"
+            let player = 
+                spawn system "player" 
+                    (fun mailbox -> 
+                        let rec loop(users, reporting) = actor {                            
+                            let! message = mailbox.Receive()
+                            users <! message
+                            reporting <! message
+                            return! loop(users, reporting)
+                        }
+                        loop(createUsers mailbox, createReporting mailbox))
+
+            // Start sending some play events
+            feedPlayerWithEvents player
 
             system.Shutdown()
             0
